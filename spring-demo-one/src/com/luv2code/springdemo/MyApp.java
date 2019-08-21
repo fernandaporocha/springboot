f39277a91494c9fbdd3cd39b269fabb4f1914b48
fernandaporocha <fernandaporocha@gmail.com>
2019-08-21 13:27:09 +0000

    Adding interface

--- spring-demo-one/src/com/luv2code/springdemo/MyApp.java
+++ spring-demo-one/src/com/luv2code/springdemo/MyApp.java
@@ -3,7 +3,8 @@ package com.luv2code.springdemo;
 public class MyApp {
 
 	public static void main(String[] args) {
-		BaseballCoach theCoach = new BaseballCoach();
+		
+		Coach theCoach = new BaseballCoach();
 		
 		System.out.println(theCoach.getDailyWorkout());
 
